f4b8b625112e4e11888968292824a954673eb5b0
Abhi_Aditya <123725608+javabhihek@users.noreply.github.com>
2024-06-26 01:02:54 +0530

    Add files via upload
    Uploaded linearSearch,BinarySearch and Flowchart of login process

---
+++ binarySearch.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={34,45,78,78,90,100};
+    int low=0, high=5;
+    int key,mid,r,i;
+    printf("Enter a search value:");
+    scanf("%d",&key);
+
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(arr[mid])
+    }
+}
